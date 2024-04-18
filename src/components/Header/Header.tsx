3ece803d1c2e3cc332822903947be2a28dbe1ce4
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-18 15:58:34 -0400

    feat: add Header component

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+import { Logo } from "@/components/Logo";
+import { CartButton } from "@/components/Cart";
+
+export type HeaderProps = {
+	justifyContent?: JustifyContent;
+};
+
+type JustifyContent =
+	| "flex-start"
+	| "flex-end"
+	| "space-between"
+	| "space-around";
+
+const Wrapper = styled.header<HeaderProps>`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	justify-content: ${(props) => props.justifyContent || "space-between"};
+`;
+
+function Header() {
+	return (
+		<Wrapper>
+			<Logo />
+			<CartButton />
+		</Wrapper>
+	);
+}
+
+export default Header;
---
+++ src/components/Header/index.ts
@@ -0,0 +1,1 @@
+export { type HeaderProps, default as Header } from "./Header";
