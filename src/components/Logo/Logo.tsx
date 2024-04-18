8536b867e3097ac9bee1f5608d013c5fdeb89812
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-18 15:57:30 -0400

    feat: add Logo component

---
+++ src/components/Logo/Logo.tsx
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+
+export type LogoProps = {
+	color?: string;
+};
+
+const Link = styled.a`
+	display: block;
+	text-decoration: none;
+`;
+
+const Wrapper = styled.h1<LogoProps>`
+	font-size: 1.25rem;
+	font-weight: 700;
+	color: ${(prop) => prop.color || "#fff"};
+`;
+
+export default function Logo(props: LogoProps) {
+	return (
+		<Link title="Logo da WeMovie" href="/" target="_self">
+			<Wrapper {...props}>WeMovies</Wrapper>
+		</Link>
+	);
+}
