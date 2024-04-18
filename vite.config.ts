dbad1e82083d8d2616f412985e0c9df026da5e54
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-18 14:50:15 -0400

    Initial commit

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import * as path from "path";
+import react from "@vitejs/plugin-react-swc";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+	base: "./",
+	plugins: [react()],
+	resolve: {
+		alias: [{ find: "@", replacement: path.resolve(__dirname, "src") }],
+	},
+});
