f52f94b95d57370e9c2f3d2da6df75ea9ed1d541
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-18 15:58:20 -0400

    feat: add CartButton component

---
+++ src/components/Cart/CartButton.tsx
@@ -0,0 +1,48 @@
+import styled from "styled-components";
+import { Cart } from "@/components/Icons";
+
+const Link = styled.a`
+	display: block;
+	text-decoration: none;
+`;
+
+const Wrapper = styled.div`
+	display: flex;
+	flex-direction: row;
+	align-items: center;
+	gap: 0.5rem;
+	color: var(--white);
+
+	.content {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: flex-end;
+
+		p {
+			font-size: 14px;
+			margin: 0;
+		}
+
+		span {
+			font-size: 12px;
+			color: var(--gray);
+		}
+	}
+`;
+
+function CartButton() {
+	return (
+		<Link title="Botão do Carrinho" href="/?cart" target="_self">
+			<Wrapper>
+				<div className="content">
+					<p>Meu carrinho</p>
+					<span>0 itens</span>
+				</div>
+				<Cart />
+			</Wrapper>
+		</Link>
+	);
+}
+
+export default CartButton;
