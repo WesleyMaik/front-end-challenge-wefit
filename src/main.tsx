fe5e483eb911d746e3f2f0081fa4138720bfb3a9
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-18 22:59:02 -0400

    feat: add react-query and movie providers

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,22 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "@/routes/App";
+
+import { CartProvider } from "@/contexts/CartContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "@/libs/react-query";
+
 import "@/style/global.css";
-import { CartProvider } from "./contexts/CartContext";
+import { MovieProvider } from "./contexts/MovieContext";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<CartProvider items={[]}>
-			<App />
-		</CartProvider>
+		<QueryClientProvider client={queryClient}>
+			<MovieProvider>
+				<CartProvider items={[]}>
+					<App />
+				</CartProvider>
+			</MovieProvider>
+		</QueryClientProvider>
 	</React.StrictMode>
 );
