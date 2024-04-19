59159e0909900999c2b1f2903d70feb78eba6f8d
WesleyMaik <wesleynamikaze404@gmail.com>
2024-04-19 13:52:25 -0400

    feat: add Cart route component

---
+++ src/Cart.tsx
@@ -0,0 +1,14 @@
+import Resume from "@/components/Cart/Resume";
+import { Container } from "@/components/Container";
+import { Header } from "@/components/Header";
+
+function Cart() {
+	return (
+		<Container>
+			<Header />
+			<Resume />
+		</Container>
+	);
+}
+
+export default Cart;
